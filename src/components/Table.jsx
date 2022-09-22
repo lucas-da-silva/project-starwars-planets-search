51115bf5db2b5ee070f7c9f5dcba079a72099933
lucas-da-silva <lucassouza00012345@gmail.com>
2022-09-22 19:45:06 -0300

    fix bugs

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -4,8 +4,65 @@ import StarWarsContext from '../contexts/StarWarsContext';
 import '../styles/Table.css';
 import TableRender from './TableRender';
 
+const HIGH_VALUE = 1;
+const LOWER_VALUE = -1;
+
 function Table() {
-  const { planets, filterByNumericValues, removeFilter } = useContext(StarWarsContext);
+  const { planets, filterByName, filterByNumericValues,
+    removeFilter, filterBySort } = useContext(StarWarsContext);
+
+  const filterNumericPlanets = (planet, column, comparison, number) => {
+    if (comparison === 'maior que') return Number(planet[column]) > Number(number);
+    if (comparison === 'menor que') return Number(planet[column]) < Number(number);
+    if (comparison === 'igual a') return Number(planet[column]) === Number(number);
+  };
+
+  const sortPlanets = (planetsToSort, column, sort) => {
+    if (sort === 'ASC') {
+      return planetsToSort.sort(
+        (a, b) => {
+          if (a[column] === 'unknown') return HIGH_VALUE;
+          if (b[column] === 'unknown') return LOWER_VALUE;
+          return a[column] - b[column];
+        },
+      );
+    }
+    if (sort === 'DESC') {
+      return planetsToSort.sort(
+        (a, b) => {
+          if (a[column] === 'unknown') return HIGH_VALUE;
+          if (b[column] === 'unknown') return LOWER_VALUE;
+          return b[column] - a[column];
+        },
+      );
+    }
+  };
+
+  let filteredPlanets = planets;
+
+  if (filterByName.name) {
+    const nameFilter = filterByName.name.toLowerCase();
+    filteredPlanets = filteredPlanets.filter(
+      ({ name }) => name.toLowerCase().includes(nameFilter),
+    );
+  }
+
+  if (filterByNumericValues.length) {
+    filteredPlanets = filteredPlanets.filter((planet) => {
+      let value = true;
+      filterByNumericValues.forEach(({ column, comparison, number }) => {
+        if (planet[column] === 'unknown') value = false;
+        const result = filterNumericPlanets(planet, column, comparison, number);
+        if (!result) value = false;
+      });
+      return value;
+    });
+  }
+
+  if (filterBySort.order) {
+    const { order: { column, sort } } = filterBySort;
+    filteredPlanets = sortPlanets(filteredPlanets, column, sort);
+  }
 
   return (
     <section className="table-container">
@@ -41,7 +98,7 @@ function Table() {
           </tr>
         </thead>
         <tbody>
-          {planets.map((planet) => (
+          {filteredPlanets.map((planet) => (
             <TableRender key={ planet.name } planet={ planet } />
           ))}
         </tbody>
